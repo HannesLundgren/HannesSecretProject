4c369ac8d1c7161b76569cfb707ebb51a854e85d
amadeus.johansson <amadeus.johansson@UPP-R9Z934H.learnet.se>
2017-02-28 08:07:05 +0100

    Updated FIFO
    Lagt till en setter. Kommer inte ihåg hur lång kön inuti salongen fick
    vara men den är just nu 6. Titta gärna igenom den utifall jag missat
    något.

--- src/state/FIFO.java
+++ src/state/FIFO.java
@@ -1,5 +1,51 @@
 package state;
 
-public class FIFO {
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+public class FIFO{
+	private ArrayList<Object> fifoList = new ArrayList<Object>();
+	private int currentSize = 0;
 	
+	public void add(Object arg0) {
+		fifoList.add(arg0);
+		++currentSize;
+	}
+	
+	public boolean isEmpty() {
+		return size() == 0;
+	}
+
+	public Object getFirst() throws NoSuchElementException {
+		if(!this.isEmpty()){
+			return fifoList.get(0);
+		} else {
+			throw new NoSuchElementException();
+		}
+	}
+	
+	public void setFirst(Object object){
+		Object temp = fifoList.get(0);
+		fifoList.set(0, object);
+		if(currentSize >= 6){
+			return;
+		} else {
+			fifoList.add(temp);
+		}
+	}
+	
+
+	public void removeFirst() throws NoSuchElementException {
+		if(!this.isEmpty()){
+			fifoList.remove(0);
+			--currentSize;
+		} else {
+			throw new NoSuchElementException();
+		}
+	}
+
+	public int size() {
+		return currentSize;
+	}
+
 }
