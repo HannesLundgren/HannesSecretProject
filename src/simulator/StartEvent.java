f0f091ea0bcd3ff9b8c9dbd549e8f3d8993276e6
HannesLundgren <hanneslundgren@pub108-209.pub.ltu.se>
2017-02-28 10:07:46 +0100

    Event changes

--- src/simulator/StartEvent.java
+++ src/simulator/StartEvent.java
@@ -1,5 +1,22 @@
 package simulator;
+/**
+ * Class that represent a general start event
+ * @author hanneslundgren
+ *
+ */
+public class StartEvent extends Event{
 
-public class StartEvent {
+	@Override
+
+	public void execute() {
+	
+		
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
